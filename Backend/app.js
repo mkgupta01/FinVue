7b7e9408a5d69abc804238bbbd8d1bdbc0d0d1b1
Mayank Kumar Gupta <66107976+mkgupta01@users.noreply.github.com>
2024-05-23 03:35:19 +0530

    Update app.js
    Updated CORS

--- Backend/app.js
+++ Backend/app.js
@@ -8,8 +8,9 @@ const app = express();
 
 //middleware
 app.use(cors({
-    origin: "https://fin-vue.vercel.app",
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
+    credentials: true, 
+    origin: true, 
+    exposedHeaders: '*'
 }))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
